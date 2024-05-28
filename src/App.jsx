5a7c3fa79ca577e672de3b33e16d91931df7baf2
Anthony <141275535+Anthony-0801@users.noreply.github.com>
2024-05-28 14:03:14 +0000

    adding my info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Anthony A. Cabulang",
+  title: "Aspiring Web Developer and Data Analyst",
+  email: "anthony.cabulang1@gmail.com",
+  gitHub: "https://github.com/Anthony-0801",
+  instagram: "azyx_____",
+  linkedIn: "https://www.linkedin.com/in/anthony-cabulang-4b8163299/",
 };
 
 const primaryColor = "#4E567E";
